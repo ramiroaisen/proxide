16bb90dd7237c49a8360a42733b4939a0620b231
ramiroaisen <52116153+ramiroaisen@users.noreply.github.com>
2024-07-21 23:04:14 -0300

    tests: fix balance lesat connections test

--- tests/balance-least-connections.rs
+++ tests/balance-least-connections.rs
@@ -39,9 +39,13 @@ fn balance_least_connections() {
             let (stream, _) = server.accept().await.unwrap();
             let (s0, s1, s2, s3) = (s0.clone(), s1.clone(), s2.clone(), s3.clone());
             tokio::spawn(async move {
-              let _ws = async_tungstenite::tokio::accept_async(stream)
-                .await
-                .unwrap();
+              let _ws = match async_tungstenite::tokio::accept_async(stream).await {
+                Ok(ws) => ws,
+                Err(e) => {
+                  log::warn!("error accepting ws stream: {e}");
+                  return;
+                }
+              };
 
               match i {
                 0 => s0.fetch_add(1, Ordering::SeqCst),
@@ -51,7 +55,7 @@ fn balance_least_connections() {
                 _ => unreachable!(),
               };
 
-              tokio::time::sleep(Duration::from_millis(5000)).await;
+              tokio::time::sleep(Duration::from_millis(60_000)).await;
             });
           }
         });
@@ -60,18 +64,18 @@ fn balance_least_connections() {
       tokio::time::sleep(Duration::from_millis(100)).await;
 
       // clients
-      for i in 1..=10 {
-        for j in 1..=4 {
+      for _ in 1..=10 {
+        for _ in 1..=4 {
+          tokio::time::sleep(Duration::from_millis(100)).await;
           tokio::spawn(async move {
-            tokio::time::sleep(Duration::from_millis(50 * i * j)).await;
             let ws = ws(&format!("ws://127.0.0.1:2060{p}/")).await.unwrap();
-            tokio::time::sleep(Duration::from_millis(5000)).await;
+            tokio::time::sleep(Duration::from_millis(60_000)).await;
             drop(ws);
           });
         }
       }
 
-      thread::sleep(Duration::from_millis(3000));
+      thread::sleep(Duration::from_millis(100));
 
       macro_rules! check {
         ($e:ident) => {
