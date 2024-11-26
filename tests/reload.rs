e5bbd8b70675f2579373f79d94ea32dc428372c8
ramiroaisen <52116153+ramiroaisen@users.noreply.github.com>
2024-11-26 18:10:43 -0300

    core: add reload test

---
+++ tests/reload.rs
@@ -0,0 +1,165 @@
+mod common;
+use axum::{body::Body, response::Response};
+use clap::Parser;
+use common::{block_on, dir};
+use http::{header::CONNECTION, HeaderValue};
+use proxide::cli::{self, args::Args};
+
+#[test]
+fn reload() {
+  block_on(async move {
+    let dir = dir();
+
+    let cfg_path = dir.file("config.yml");
+    let pidfile = dir.file("proxide.pid");
+
+    let stream_port_1 = 25101;
+    let http_port_1 = 25201;
+
+    let stream_port_2 = 25102;
+    let http_port_2 = 25202;
+
+    let config_src = include_str!("reload.yml");
+
+    let config_1 = config_src
+      .replace("%PIDFILE%", &pidfile)
+      .replace("%STREAM_PORT%", &stream_port_1.to_string())
+      .replace("%HTTP_PORT%", &http_port_1.to_string());
+
+    std::fs::write(&cfg_path, &config_1).unwrap();
+
+    let h_stream_1 = || async {
+      let mut res = Response::new(Body::empty());
+      res
+        .headers_mut()
+        .insert(CONNECTION, HeaderValue::from_static("close"));
+      res
+        .headers_mut()
+        .insert("x-backend", HeaderValue::from_static("stream-1"));
+      res
+        .headers_mut()
+        .insert("x-test", HeaderValue::from_static("reload"));
+      res
+    };
+
+    let h_http_1 = || async {
+      let mut res = Response::new(Body::empty());
+      // res
+      //   .headers_mut()
+      //   .insert(CONNECTION, HeaderValue::from_static("close"));
+      res
+        .headers_mut()
+        .insert("x-backend", HeaderValue::from_static("http-1"));
+      res
+        .headers_mut()
+        .insert("x-test", HeaderValue::from_static("reload"));
+      res
+    };
+
+    let h_stream_2 = || async {
+      let mut res = Response::new(Body::empty());
+      res
+        .headers_mut()
+        .insert(CONNECTION, HeaderValue::from_static("close"));
+      res
+        .headers_mut()
+        .insert("x-backend", HeaderValue::from_static("stream-2"));
+      res
+        .headers_mut()
+        .insert("x-test", HeaderValue::from_static("reload"));
+      res
+    };
+
+    let h_http_2 = || async {
+      let mut res = Response::new(Body::empty());
+      // res
+      //   .headers_mut()
+      //   .insert(CONNECTION, HeaderValue::from_static("close"));
+      res
+        .headers_mut()
+        .insert("x-backend", HeaderValue::from_static("http-2"));
+      res
+        .headers_mut()
+        .insert("x-test", HeaderValue::from_static("reload"));
+      res
+    };
+
+    let server_stream_1 = axum::Router::new().route("/", axum::routing::get(h_stream_1));
+    let server_http_1 = axum::Router::new().route("/", axum::routing::get(h_http_1));
+    let server_stream_2 = axum::Router::new().route("/", axum::routing::get(h_stream_2));
+    let server_http_2 = axum::Router::new().route("/", axum::routing::get(h_http_2));
+
+    tokio::spawn(async move {
+      let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{stream_port_1}"))
+        .await
+        .unwrap();
+      axum::serve(listener, server_stream_1).await.unwrap();
+    });
+
+    tokio::spawn(async move {
+      let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{http_port_1}"))
+        .await
+        .unwrap();
+      axum::serve(listener, server_http_1).await.unwrap();
+    });
+
+    tokio::spawn(async move {
+      let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{stream_port_2}"))
+        .await
+        .unwrap();
+      axum::serve(listener, server_stream_2).await.unwrap();
+    });
+
+    tokio::spawn(async move {
+      let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{http_port_2}"))
+        .await
+        .unwrap();
+      axum::serve(listener, server_http_2).await.unwrap();
+    });
+
+    let args = Args::try_parse_from(["proxide", "start", "--config", &cfg_path]).unwrap();
+    std::thread::spawn(move || {
+      cli::run(args).unwrap();
+    });
+
+    let client = reqwest::Client::new();
+
+    for i in 0..1000 {
+      let port = if i % 2 == 0 { 25100 } else { 25200 };
+      let backend = if i % 2 == 0 { "stream-1" } else { "http-1" };
+      let res = client
+        .get(&format!("http://127.0.0.1:{port}"))
+        .send()
+        .await
+        .unwrap();
+      assert_header!(res, "x-test", "reload");
+      assert_header!(res, "x-backend", backend);
+    }
+
+    let new_config = config_src
+      .replace("%PIDFILE%", &pidfile)
+      .replace("%STREAM_PORT%", &stream_port_2.to_string())
+      .replace("%HTTP_PORT%", &http_port_2.to_string());
+    std::fs::write(&cfg_path, &new_config).unwrap();
+
+    let args = Args::try_parse_from([
+      "proxide", "signal", "--config", &cfg_path, "--signal", "reload",
+    ])
+    .unwrap();
+    cli::run(args).unwrap();
+
+    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+
+    for i in 0..1000 {
+      let port = if i % 2 == 0 { 25100 } else { 25200 };
+      let backend = if i % 2 == 0 { "stream-2" } else { "http-2" };
+      let res = client
+        .get(&format!("http://127.0.0.1:{port}"))
+        .send()
+        .await
+        .unwrap();
+      assert_header!(res, "x-test", "reload");
+      assert_header!(res, "x-backend", backend);
+    }
+  })
+}
