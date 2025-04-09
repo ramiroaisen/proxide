82d454a63ff03fc789648ef4b354535f3b1e73a2
ramiroaisen <52116153+ramiroaisen@users.noreply.github.com>
2025-04-09 15:05:36 -0300

    change test port

--- tests/upstream-down.rs
+++ tests/upstream-down.rs
@@ -9,7 +9,9 @@ fn upstream_down() {
   block_on(async move {
     for balance in ["round-robin", "random", "ip-hash", "least-connections"] {
       for _ in 0..50 {
-        let res = get(&format!("http://127.0.0.1:21600/balance-{balance}")).await.unwrap();
+        let res = get(&format!("http://127.0.0.1:21605/balance-{balance}"))
+          .await
+          .unwrap();
         assert_status!(res, OK);
         assert_header!(res, "x-test", "upstream-down");
         assert_header!(res, "x-balance", balance);
